a3bf22d1070cee475ed3159926c4f55d73f8c6c1
Omkulthoom <sheng.na.u@hotmail.com>
2020-05-04 06:53:36 +0300

    assignment 1-2

--- work-2/ContentView.swift
+++ work-2/ContentView.swift
@@ -10,9 +10,138 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack(spacing: 15.0){
+            Spacer()
+        Text("555")
+            .offset(x: 100)
+        .font (.system (size:100))
+            
+
+            
+            HStack(spacing: 10)
+            {
+                Text ("C")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("±")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("%")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("÷")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.orange)
+                .clipShape(Capsule())
+                
+            }
+            
+            HStack{
+                Text ("7")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("8")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("9")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("x")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.orange)
+                .clipShape(Capsule())
+            }
+            HStack{
+                Text ("4")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("5")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("6")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("-")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.orange)
+                .clipShape(Capsule())
+            }
+            HStack{
+                Text ("1")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("2")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("3")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("+")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.orange)
+                .clipShape(Capsule())
+            }
+            HStack(spacing: 16.0){
+                Text ("0")
+                .bold()
+                .frame(width: 160, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text (".")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.gray)
+                .clipShape(Capsule())
+                Text ("-")
+                .bold()
+                .frame(width: 85, height: 85)
+                .background(Color.orange)
+                .clipShape(Capsule())
+            }
+
+
+        }
+            
+        .padding(.all, 30.0)
+            .font (.system (size:30))
+        .foregroundColor(.white)
+        .background(Color (#colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1529411765, alpha: 1)))
+        .edgesIgnoringSafeArea(.all)
+        
+        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
